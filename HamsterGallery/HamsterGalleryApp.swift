1d0a95f3022598b6b5a88c5d38839c61ad2853a6
AliAlhasan2023 <Ali.alhasan743@gmail.com>
2024-01-24 14:58:14 +0100

    Initial Commit

---
+++ HamsterGallery/HamsterGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamsterGalleryApp.swift
+//  HamsterGallery
+//
+//  Created by Ali alhasan on 2024-01-24.
+//
+
+import SwiftUI
+
+@main
+struct HamsterGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
